706d1f304aa38a31856cdf5af10de9e0eeecdb58
kareckyy <karol.jagodzinski19@gmail.com>
2024-02-24 15:31:44 +0100

    Dodaj pliki projektów.

---
+++ Cars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wypozyczalnia
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type {  get; set; }
+        public string Engine {  get; set; }
+        public string Status { get; set; }  
+        public decimal Price {  get; set; }
+
+    }
+}
---
+++ Clients.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Client
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DrivingLicenseDate { get; set; }
+}
+
